54ca4fb1e4f58e9b3fca51fea7ae646d049a6936
Kelsey Z <zahandke@gmail.com>
2020-08-04 13:30:27 -0600

    added more photos to team page. worked on nav menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,14 @@ $(document).on("click", "[data-toggle=\"lightbox\"]", function (event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('.logo').fadeOut();
+      $(".navbar").addClass("navbar-scroll-effect");
+    } else {
+      $('.logo').fadeIn();
+      $(".navbar").removeClass("navbar-scroll-effect");
+    }
+  });
+
